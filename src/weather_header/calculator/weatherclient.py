28f9bc072b09c1ba798453df3eba06304e139142
donngas <geunhyeongsam@gmail.com>
2026-01-19 03:06:44 +0900

    feat: add get_timezone method for better params in get_weather

--- src/weather_header/calculator/weatherclient.py
+++ src/weather_header/calculator/weatherclient.py
@@ -2,6 +2,7 @@ import openmeteo_requests
 import requests_cache
 from retry_requests import retry
 from geopy import Nominatim
+from timezonefinder import TimezoneFinder
 
 URL = "https://api.open-meteo.com/v1/forecast"
 
@@ -19,6 +20,7 @@ class WeatherClient:
         # clients
         self.weather_client = openmeteo_requests.Client(session=retry_session)
         self.geo_client = Nominatim(user_agent="weather-header")
+        self.timezone_finder = TimezoneFinder()
 
     def get_weather(self, latitude: float, longitude: float) -> dict:
         """
@@ -27,7 +29,11 @@ class WeatherClient:
         params = {
             "latitude": latitude,
             "longitude": longitude,
+            "daily": ["sunrise", "sunset"],
             "current": "weather_code",
+            "timezone": self.get_timezone(latitude, longitude),
+            "past_days": 1,
+            "forcast_days": 1,
         }
         responses = self.weather_client.weather_api(URL, params=params)
         return responses[0]
@@ -39,3 +45,9 @@ class WeatherClient:
         location = self.geo_client.reverse(f"{latitude}, {longitude}", exactly_one=True)
         # TODO: handle cases where there might be no city key.
         return location.raw["address"]["city"]
+
+    def get_timezone(self, latitude: float, longitude: float) -> str:
+        """
+        Get timezone from coordinates.
+        """
+        return self.timezone_finder.timezone_at(lat=latitude, lng=longitude)
