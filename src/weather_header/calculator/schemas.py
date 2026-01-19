5f0ec7e930e6696c660caec1a4e3de100108f96b
donngas <geunhyeongsam@gmail.com>
2026-01-19 18:44:18 +0900

    feat: create DTO for StateResolver results

--- src/weather_header/calculator/schemas.py
+++ src/weather_header/calculator/schemas.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel, Field
 from typing import List
+from enum import Enum
+
+
+# Pydantic models for DTO between WeatherClient and StateResolver
 
 
 class Time(BaseModel):
@@ -23,3 +27,42 @@ class WeatherData(BaseModel):
     sunrises: List[Time]
     sunsets: List[Time]
     local_time: Time
+
+
+# Pydantic models for StateResolver results
+
+
+class WeatherType(str, Enum):
+    """
+    Enum for weather types.
+    """
+
+    SUNNY = "sunny"
+    OVERCAST = "overcast"
+    RAIN = "rain"
+    SNOW = "snow"
+
+
+class TimeType(str, Enum):
+    """
+    Enum for time types.
+    """
+
+    DAY = "day"
+    NIGHT = "night"
+    GOLDEN = "golden"
+
+
+class UserState(BaseModel):
+    """
+    Class for resolved final weather information for user.
+    """
+
+    weather: WeatherType
+    time: TimeType
+
+    # to determine heaviness of rain or weather
+    is_heavy: bool
+
+    # to determine lightning effects
+    is_storm: bool
